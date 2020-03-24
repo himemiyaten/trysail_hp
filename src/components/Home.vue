c6d772db6a5b3d983a98dd5ed8e3a1d806f12fd3
louiss011371 <louiss011371@hotmail.com>
2020-03-24 22:15:04 +0800

    Update home page layout

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="layout">
+    <!-- layout from Slide -->
+    <Slide>
+    </Slide>
+    <!-- layout from Slide -->
+   <!-- layout from HelloWorld2 -->
+    <HelloWorld2>
+    </HelloWorld2>
+    <!-- layout from HelloWorld2 -->
+    <!-- layout from BodyLayout -->
+      <BodyLayout>
+     </BodyLayout>
+    <!-- layout from BodyLayout -->
+  </div>
+</template>
+
+
+<script>
+  import Slide from './Slide.vue'
+  import BodyLayout from './BodyLayout.vue';
+
+export default {
+    components : { Slide, BodyLayout, },
+};
+</script>
+
+<style>
+#layout {
+  padding-left: 20%;
+  padding-right: 20%;
+}
+
+</style>
