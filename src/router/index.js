7662470e9a4961eda1b96850849c3089de0a6106
louiss011371 <louiss011371@hotmail.com>
2020-03-25 21:40:38 +0800

    Update version

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 
+
 Vue.use(Router)
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 export default new Router({
   routes: [
     {
