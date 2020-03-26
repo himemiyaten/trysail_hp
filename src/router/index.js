da0dbbcca7023e55f55b32ad5a2f5b024f9dbbaa
louiss011371 <louiss011371@hotmail.com>
2020-03-26 17:04:45 +0800

    Update imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-
-
+import Paginate from 'vuejs-paginate'
+import VuePaginate from 'vue-paginate';
+import lodash from 'lodash' 
 Vue.use(Router)
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
@@ -10,6 +11,10 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+Vue.component('paginate', Paginate)
+Vue.use(VuePaginate);
+Vue.use(lodash)
+
 export default new Router({
   routes: [
     {
