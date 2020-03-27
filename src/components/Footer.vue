91c6fbdc8b7a744181508a05d9aa4d3b167ca576
louiss011371 <louiss011371@hotmail.com>
2020-03-27 20:44:28 +0800

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,73 @@
+<template>
+    <!-- footer -->
+      <div class="footerlayout">
+        <div class="row">
+            <div class="col">
+                当サイトについて
+            </div>
+            <div class="col">
+                アカウントについて
+            </div>
+             <div class="col">
+                お支払いについて
+            </div>
+             <div class="col">
+                利用規約
+            </div>
+             <div class="col">
+                個人情報保護方針
+            </div>
+             <div class="col">
+                特商法に基づく表示
+            </div>
+             <div class="col">
+                推奨環境
+            </div>
+             <div class="col">
+                よくあるご質問
+            </div>
+             <div class="col">
+                会員退会
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                掲載されているすべてのコンテンツ(記事、画像、音声データ、映像データ等)の無断転載を禁じます。
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                ©MusicRay’n Inc.
+            </div>
+        </div>
+      </div>
+    <!-- footer -->
+</template>
+
+<script>
+
+</script>
+
+<style>
+.footerlayout {
+    background-color: #a6a6a6;
+    padding-bottom: 2%;
+}
+
+.footerlayout .row {
+    margin:0;
+    padding-top: 1%;
+    padding-left:18%;
+    padding-right:18%;
+}
+
+.footerlayout .row .col {
+    color: white;
+    text-align: center;
+    font-size: 12px;
+    margin: 0;
+    padding-left:0;
+    padding-right:0;
+}
+
+</style>
